bcf0cc6f74169a2335bbbf7f64dd74bec88b21c9
juliaknmelo <ju-kar98@hotmail.com>
2024-06-07 12:05:34 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -2,6 +2,9 @@ package com.generation.blogpessoal.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -26,6 +29,7 @@ public class Usuario {
 	@NotNull(message = "O atributo nome é obrigatório!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message="O atributo e-mail é obrigatório!")
 	@Email(message="O atributo deve ser um e-mail válido")
 	private String usuario;
@@ -37,7 +41,8 @@ public class Usuario {
 	@Size(max= 5000, message = "A foto deve ter no máximo 5000 caracteres")
 	private String foto;
 	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy= "usuario", cascade=CascadeType.REMOVE )
+	@OneToMany(fetch = FetchType.LAZY, mappedBy= "usuario", cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 	
 	
